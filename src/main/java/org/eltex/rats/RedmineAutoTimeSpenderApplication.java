857392a642f29d7efe0184cb01b3e994a51b738d
Дмитрий Виноградов <dmitriy.vinogradov@eltex.loc>
2024-01-30 11:12:29 +0700

    add time zone

--- src/main/java/org/eltex/rats/RedmineAutoTimeSpenderApplication.java
+++ src/main/java/org/eltex/rats/RedmineAutoTimeSpenderApplication.java
@@ -3,10 +3,13 @@ package org.eltex.rats;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class RedmineAutoTimeSpenderApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+7:00"));
         SpringApplication.run(RedmineAutoTimeSpenderApplication.class, args);
     }
 
