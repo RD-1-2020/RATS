c277798bf2fc1d6bdf7a350c93334b37a2d32ac0
mestr <qwe@qwe.qwe>
2025-04-22 14:56:03 +0700

    fix ssl in service

--- src/main/java/org/eltex/rats/shcedule/DailySchedule.java
+++ src/main/java/org/eltex/rats/shcedule/DailySchedule.java
@@ -3,6 +3,12 @@ package org.eltex.rats.shcedule;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +16,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.net.ssl.SSLContext;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -36,11 +43,19 @@ public class DailySchedule {
     }
 
     @Scheduled(cron = "${daily.cron}")
-    public void createDailyTimeSpent() throws UnirestException {
+    public void createDailyTimeSpent() throws Exception {
         logger.info("[DAILY SCHEDULE]: Start request for creating daily time spent...");
 
-        Unirest.setTimeouts(redmineTimeout, redmineTimeout);
-        HttpResponse<String> response = Unirest.post("http://red.eltex.loc/time_entries.xml")
+        SSLContext sslcontext = SSLContexts.custom()
+                .loadTrustMaterial(null, new TrustSelfSignedStrategy())
+                .build();
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext, SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+        CloseableHttpClient httpclient = HttpClients.custom()
+                .setSSLSocketFactory(sslsf)
+                .build();
+        Unirest.setHttpClient(httpclient);
+
+        HttpResponse<String> response = Unirest.post("https://red.eltex.loc/time_entries.xml")
                 .header("Content-Type", "application/x-www-form-urlencoded")
                 .header("Authorization", "Basic " + authToken)
                 .field("utf8", "✓")
@@ -48,7 +63,7 @@ public class DailySchedule {
                 .field("issue_id", mainVersionTask)
                 .field("time_entry[issue_id]", mainVersionTask)
                 .field("time_entry[spent_on]", redmineDateFormat.format(Calendar.getInstance().getTime()))
-                .field("time_entry[hours]", "0,5")
+                .field("time_entry[hours]", "0:30")
                 .field("time_entry[comments]", "Daily")
                 .field("time_entry[activity_id]", "10")
                 .field("continue", "Создать+и+продолжить")
